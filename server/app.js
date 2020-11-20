aacf8a952fd97b62ca84242aaf4c9753ce626658
Meenakshi Sinha <meenakshisinha2012@gmail.com>
2020-11-20 15:46:42 +0530

    Few fixes

--- server/app.js
+++ server/app.js
@@ -43,13 +43,5 @@ app.use((req, res, next) => {
   next(error);
 });
 
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    error: {
-      message: error.message
-    }
-  });
-});
 
 module.exports = app;
